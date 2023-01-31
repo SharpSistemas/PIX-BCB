33c548e4f146802456b90dca7edb429b4013a446
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-31 15:33:53 -0300

    Migração de repositório
    Migração do projeto CS.BCB.PIX do gh/SharpSistemas/SicoobAPI para gh/SharpSistemas/PIX-BCB

---
+++ CS.BCB.PIX/Models/ConsultarCobrancaImediata.cs
@@ -0,0 +1,15 @@
+﻿/**************************************\
+ * Biblioteca C# para APIs do PIX     *
+ * Autor: Rafael Estevam              *
+ *        gh/SharpSistemas/SicoobAPI  *
+\**************************************/
+using System;
+
+namespace CS.BCB.PIX.Models
+{
+    public class ConsultarCobrancaImediata : ConsultaCpfCnpj
+    {
+        public bool? locationPresente { get; set; }
+        public string? status { get; set; }
+    }
+}
---
+++ CS.BCB.PIX/Models/DadosDevedor.cs
@@ -0,0 +1,16 @@
+﻿/**************************************\
+ * Biblioteca C# para APIs do PIX     *
+ * Autor: Rafael Estevam              *
+ *        gh/SharpSistemas/SicoobAPI  *
+\**************************************/
+namespace CS.BCB.PIX.Models
+{
+    public class DadosDevedor : NomeCpfCnpj
+    {
+        public string email { get; set; }
+        public string logradouro { get; set; }
+        public string cidade { get; set; }
+        public string uf { get; set; }
+        public string cep { get; set; }
+    }
+}
