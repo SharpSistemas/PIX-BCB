65b6c86f3246d37ce34d401be1e710a2d2815e0e
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2023-01-31 15:38:10 -0300

    Altera para File-scoped

--- CS.BCB.PIX/Excecoes/ErroRequisicaoException.cs
+++ CS.BCB.PIX/Excecoes/ErroRequisicaoException.cs
@@ -3,24 +3,23 @@
  * Autor: Rafael Estevam              *
  *        gh/SharpSistemas/SicoobAPI  *
 \**************************************/
+namespace CS.BCB.PIX.Excecoes;
+
 using CS.BCB.PIX.Models;
 using System;
 
-namespace CS.BCB.PIX.Excecoes
+public class ErroRequisicaoException : Exception
 {
-    public class ErroRequisicaoException : Exception
-    {
-        public ErroRequisicao DadosErro { get; }
+    public ErroRequisicao DadosErro { get; }
 
-        public ErroRequisicaoException(ErroRequisicao erro)
-            : base(erro.Title)
-        {
-            DadosErro = erro;
-        }
-        public ErroRequisicaoException(ErroRequisicao erro, Exception innerException)
-            : base(erro.Title, innerException)
-        {
-            DadosErro = erro;
-        }
+    public ErroRequisicaoException(ErroRequisicao erro)
+        : base(erro.Title)
+    {
+        DadosErro = erro;
+    }
+    public ErroRequisicaoException(ErroRequisicao erro, Exception innerException)
+        : base(erro.Title, innerException)
+    {
+        DadosErro = erro;
     }
 }
